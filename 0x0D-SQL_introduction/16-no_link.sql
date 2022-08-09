a7fc829337f9e27b1eff8f4d0e6c164ddbe2804b
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-08-10 01:30:56 +0300

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,3 @@
 -- A script that lists all records of the table second_table of the database in your MySQL server.
-SELECT score, name FROM second_table WHERE name != '';
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+
