b7151fa8f2b0a223e06643d8b6537ea006c65ef0
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-08-09 21:49:17 +0300

    forgot comma

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- A script that creates a table called first_table in the current database in your MySQL server and doesn't fail if table already existed.
 CREATE TABLE if not exists first_table(
-id INT
+id INT,
 name VARCHAR(256)
 );
