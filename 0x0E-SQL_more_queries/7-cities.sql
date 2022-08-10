f12ff102b01c1be1380cb81d340b24ae603bc2b9
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-08-10 22:44:52 +0300

    Create 7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- Ascript that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+id PRIMARY KEY AUTO_INCREMENT,
+state_id FOREIGN KEY REFERENCES states(id) NOT NULL,
+name VARCHAR(256) NOT NULL;
