5fd023ac4b049e43b81e269876b1cdab2b7e39df
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-08-11 16:55:24 +0300

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,3 @@
 -- A script that lists all cities contained in the database.
-SELECT id, name, ( SELECT name FROM states WHERE state_id = id ) AS name FROM cities;
+SELECT cities.id, cities.name, states.name FROM cities
+INNER JOIN states ON cities.state_id = states.id;
